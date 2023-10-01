9c690275b7bdc296481bbdcabcbb91f1f0c1ba23
Shehryar Nasir <shehryarnasir98@gmail.com>
2023-10-01 21:07:44 +0500

    update

--- src/components/Moviecard.jsx
+++ src/components/Moviecard.jsx
@@ -8,10 +8,10 @@ function Moviecard({ movie }) {
     ? ` https://image.tmdb.org/t/p/w500/${poster_path}`
     : { th };
   return (
-    <div className="cardmain lg:w-[350px] lg:h-[600px]  w-[370px] h-[500px] mt-1 ml-1 lg:ml-28 lg:mt-16  bg-gray-900 rounded flex flex-col border-2 border-black dark:border-none  overflow-hidden">
+    <div className="cardmain lg:w-[350px] lg:h-[600px]  w-[350px] h-[500px] mt-1 ml-1 lg:ml-28 lg:mt-16  bg-gray-900 rounded flex flex-col border-2 border-black dark:border-none  overflow-hidden">
       <Link to={`/movie/${id}`}>
         <div className="img h-[80%] w-[100%] ">
-          <img src={image} alt="pic" className="h-[500px] w-[600px] " />
+          <img src={image} alt="pic" className="h-[500px] w-[350px] " />
         </div>
       </Link>
       <div className="cont w-[100%] h-[40%]">
